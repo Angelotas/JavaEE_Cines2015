7081b6aa0196cd8ca856ad2ac2e8eadefb06e990
Angelotas <angel.martin.bartolome.95@gmail.com>
2016-04-18 00:13:41 +0200

    Esta es la primera versión de cines2015 incluyendo  faceFlow, pero hay un error en la clase Booking, cuando lo arregle crearé la nueva versión.

---
+++ src/main/java/com/mycompany/cines2015/booking/Booking.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cines2015.booking;
+
+import com.mycompany.cines2015.entities.Movie;
+import com.mycompany.cines2015.entities.ShowTiming;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.StringTokenizer;
+import javax.faces.flow.FlowScoped;
+import javax.inject.Named;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.NamedQuery;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+/**
+ *
+ * @author Ángel
+ */
+@Named  //clase EL inyectable
+@FlowScoped("booking")  //bean de tipo Flow
+@PersistenceContext  //una entidad siempre se maneja en contexto de persistencia
+@NamedQuery(name = "ShowTiming.findByMovieAndTimingId", query = "SELECT s "
+        + "FROM ShowTiming s WHERE s.movieId.id = :movieId AND s.timingId.id = :timingId")
+        //Query de búsqueda
+
+public class Booking implements Serializable{
+    
+    //la clase Flow Scoped de soporte a Booking.xhtml
+    
+    int movieId;
+    EntityManager em;
+    String startTime;
+    int startTimeId;
+    private double cantidad;
+    private String tarjeta = "";
+    private Date fecha = new Date();
+
+    public EntityManager getEm() {
+        return em;
+    }
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getTarjeta() {
+        return tarjeta;
+    }
+
+    public void setTarjeta(String tarjeta) {
+        this.tarjeta = tarjeta;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+    public Booking(){
+        
+    }
+    
+    //MÉTODO QUE NOS DEVUELVE EL NOMBRE DE LA PELÍCULA A PARTIR DE SU ID
+    public String getMovieName() {
+        try {
+            //accede a la clase entidad Movie, mediante persistenceContext y así acceder
+            //a la base de datos de las películas
+            return em.createNamedQuery("Movie.findById", Movie.class)
+                .setParameter("id", movieId)
+                .getSingleResult()
+                .getName();
+        } catch (NoResultException e) {
+            return "";
+        }
+    }
+    
+    public String getStartTime() {
+        return startTime;
+    }
+    
+    public void setStartTime(String startTime) {
+        //TOKENIZAR POR COMAS Y GUARDAR LAS DOS VARIABLES
+        StringTokenizer tokens = new StringTokenizer(startTime, ",");
+        startTimeId = Integer.parseInt(tokens.nextToken());
+        this.startTime = tokens.nextToken();
+    }
+    
+    public int getStartTimeId() {
+        return startTimeId;
+    }
+    
+    //Encuentra el primer cine que proyecte la película
+    //seleccionada en la hora seleccionada
+    public String getTheater() {
+        try {
+            List<ShowTiming> list
+                = em.createNamedQuery("ShowTiming.findByMovieAndTimingId",
+                ShowTiming.class)
+                .setParameter("movieId", movieId)
+                .setParameter("timingId", startTimeId)
+                .getResultList();
+            if (list.isEmpty()) {
+                return "none"; //si la lista esta vacía será necesario seleccionar otra hora
+            }
+            return list
+                .get(0)
+                .getTheaterId()
+                .getId()
+                .toString();
+            //si la lista no esta vacía seleccinamos el primer resultado de cine y pasamos a la 
+            //ventana de confirmación
+        } catch (NoResultException e) {
+            return "none";
+        }
+    }
+}
