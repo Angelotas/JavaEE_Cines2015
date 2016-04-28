20337a371724205eba3218627fd3ee65fb3b8f23
Angelotas <angel.martin.bartolome.95@gmail.com>
2016-04-28 12:37:57 +0200

    Nuevo cambio peliculas

---
+++ src/main/java/com/mycompany/cines2015/client/MovieBackingBean.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cines2015.client;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Ángel
+ */
+@Named
+@SessionScoped
+public class MovieBackingBean implements Serializable{
+
+    
+    int movieId;
+    
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+    
+}
